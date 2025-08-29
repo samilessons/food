1a11c9f149749e13dd421d6fc33374e4fdc40dd4
samilessons <samisecblog@gmail.com>
2025-08-29 23:15:54 +0400

    set comments block start/end and created timer logic

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", function () {
+  // tabs start
   const tabHeadersParent = document.querySelector(".tabheader__items");
   const tabHeaders = tabHeadersParent.querySelectorAll(".tabheader__item ");
   const tabContents = document.querySelectorAll(".tabcontent");
@@ -35,4 +36,57 @@ window.addEventListener("DOMContentLoaded", function () {
       }
     }
   });
-});
+  // tabs end
+
+  // timer start
+  const endTime = "2025-08-31 05:21:59";
+
+  function getTimeRemaining(endTime) {
+    const total = Date.parse(endTime) - Date.parse(new Date());
+    let days, hours, minutes, seconds;
+
+    if (total <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      days = Math.floor(total / (1000 * 60 * 60 * 24));
+      hours = Math.floor(total / (1000 * 60 * 60) % 24);
+      minutes = Math.floor((total / 1000 / 60) % 60);
+      seconds = Math.floor((total / 1000) % 60);
+    }
+
+    return { total, days, hours, minutes, seconds };
+  }
+
+  function setZero(n) {
+    return n >= 0 && n < 10 ? `0${n}` : n;
+  }
+
+  function setClock(selector, endTime) {
+    const timer = document.querySelector(selector);
+    const daysElem = timer.querySelector("#days");
+    const hoursElem = timer.querySelector("#hours");
+    const minutesElem = timer.querySelector("#minutes");
+    const secondsElem = timer.querySelector("#seconds");
+
+    const timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const { total, days, hours, minutes, seconds } = getTimeRemaining(endTime);
+
+      daysElem.textContent = setZero(days);
+      hoursElem.textContent = setZero(hours);
+      minutesElem.textContent = setZero(minutes);
+      secondsElem.textContent = setZero(seconds);
+
+      if (total <= 0) clearInterval(timeInterval);
+    }
+  }
+
+  setClock(".timer", endTime);
+  // timer end
+}); 
