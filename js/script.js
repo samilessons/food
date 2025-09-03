edce15d00e538aee938ce57c984a5ff590f40476
samilessons <samisecblog@gmail.com>
2025-09-03 22:07:12 +0400

    modified modal and added open by timeout and by scroll

--- js/script.js
+++ js/script.js
@@ -95,25 +95,39 @@ window.addEventListener("DOMContentLoaded", function () {
   const closeModalTrigger = document.querySelector("[data-modal-close]");
   const modal = document.querySelector(".modal");
 
+  const modalTimerId = setTimeout(openModal, 320000);
+
+  function closeModal() {
+    modal.classList.add("hidden");
+    modal.classList.remove("show");
+    document.body.style.overflowY = "auto";
+    clearTimeout(modalTimerId);
+  }
+
+  function openModal() {
+    modal.classList.remove("hidden");
+    modal.classList.add("show");
+    document.body.style.overflowY = "hidden";
+    clearTimeout(modalTimerId);
+  }
+
+  function showModalByScroll() {
+    if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+      openModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  }
+
   if (!modal.matches(".hidden") && !modal.matches(".show")) {
     modal.classList.add("hidden");
   }
 
   openModalTriggers.forEach(trigger => {
     trigger.addEventListener("click", () => {
-      if (modal.classList.contains("hidden")) {
-        modal.classList.remove("hidden");
-        modal.classList.add("show");
-        document.body.style.overflowY = "hidden";
-      }
+      if (modal.classList.contains("hidden")) openModal();
     });
   });
 
-  function closeModal() {
-    modal.classList.add("hidden");
-    modal.classList.remove("show");
-    document.body.style.overflowY = "auto";
-  }
 
   closeModalTrigger.addEventListener("click", () => {
     if (modal.classList.contains("show")) closeModal();
@@ -125,6 +139,8 @@ window.addEventListener("DOMContentLoaded", function () {
 
   document.addEventListener("keydown", (e) => {
     if (e.key === 'Escape' && modal.matches(".show")) closeModal();
-  })
+  });
+
+  window.addEventListener("scroll", showModalByScroll);
   // modal end
 }); 
